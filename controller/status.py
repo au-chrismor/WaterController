944cefd0a95ecf921b2d824040e57cc7b4960999
Christopher Moran <chris.moran@emergent.com.au>
2023-10-20 18:56:34 +1100

    Adding mqtt check

--- controller/status.py
+++ controller/status.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, render_template
 from controller.db import get_db
 from controller.auth import login_required
-
+from paho.mqtt import client as mqtt
+import random
 
 bp = Blueprint('status', __name__)
 
@@ -14,7 +15,8 @@ def index():
                            tanklevel=80,
                            controllers=controller_count(),
                            ports=port_count(),
-                           enabled_ports=enabled_port_count())
+                           enabled_ports=enabled_port_count(),
+                           msg_queue=test_message_broker())
 
 
 def controller_count():
@@ -33,3 +35,26 @@ def enabled_port_count():
     db = get_db()
     count = db.execute('SELECT COUNT(port_name) FROM port WHERE enabled=1').fetchone()
     return count
+
+
+def test_message_broker():
+    broker = 'localhost'
+    port = 1883
+    topic = '$ACTION/#'
+    try:
+        client_id =f'python-mqtt-{random.randint(0, 1000)}'
+        client = mqtt.Client(client_id=client_id, userdata=None, protocol=mqtt.MQTTv311, transport='tcp')
+        client.username_pw_set('heartbeat', 'heartbeat')
+        rc = client.connect(broker, port)
+        if rc is None:
+            return 0
+        if rc == 0:
+            client.disconnect()
+            return 1
+        else:
+            return 0
+
+    except Exception as ex:
+        print(f'test_message_broker(): Exception {ex} occured')
+        return False
+
